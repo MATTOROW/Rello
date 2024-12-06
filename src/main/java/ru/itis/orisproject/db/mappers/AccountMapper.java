3e441dd1c81a0094a5d271e36e217929dfada59f
Nikita Arkadyevich <cacadada26@yandex.ru>
2024-12-06 21:08:47 +0300

    Initted db helper, added model Account, created dao for account

---
+++ src/main/java/ru/itis/orisproject/db/mappers/AccountMapper.java
@@ -0,0 +1,22 @@
+package ru.itis.orisproject.db.mappers;
+
+import ru.itis.orisproject.models.Account;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountMapper implements RowMapper<Account> {
+    @Override
+    public Account mapRow(ResultSet resultSet) {
+        try {
+            String username = resultSet.getString(1);
+            String password = resultSet.getString(2);
+            String email = resultSet.getString(3);
+            String icon_path = resultSet.getString(4);
+            String session_id = resultSet.getString(5);
+            return new Account(username, password, email, icon_path, session_id);
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+}
