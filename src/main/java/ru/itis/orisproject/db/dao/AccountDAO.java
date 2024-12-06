9fb00f5dd37bc7c17352fae9a0bde369b13fce82
Nikita Arkadyevich <cacadada26@yandex.ru>
2024-12-06 21:59:20 +0300

    Renaming method name

--- src/main/java/ru/itis/orisproject/db/dao/AccountDAO.java
+++ src/main/java/ru/itis/orisproject/db/dao/AccountDAO.java
@@ -77,7 +77,7 @@ UPDATE accounts SET username = ?, password = ?, email = ?, icon_path = ?, rmmt =
         }
     }
 
-    public int updateSessionByUsername(String username, String rmmt) {
+    public int updateRmmtnByUsername(String username, String rmmt) {
         try {
             PreparedStatement preparedStatement = dbConnection
                     .getConnection()
