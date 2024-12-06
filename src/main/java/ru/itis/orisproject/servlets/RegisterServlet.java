b3163941e62618db787a6446f95bebefeab47e72
Nikita Arkadyevich <cacadada26@yandex.ru>
2024-12-06 14:34:28 +0300

    Added register page. Changed logo size.

---
+++ src/main/java/ru/itis/orisproject/servlets/RegisterServlet.java
@@ -0,0 +1,22 @@
+package ru.itis.orisproject.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/register.html").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
