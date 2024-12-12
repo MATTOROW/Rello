c71f098ae8a4793df3f8fe808a8c772f9deb95ca
Nikita Arkadyevich <cacadada26@yandex.ru>
2024-12-12 20:28:47 +0300

    Changed database and it's logic.

--- src/main/java/ru/itis/orisproject/db/dao/AccountDAO.java
+++ src/main/java/ru/itis/orisproject/db/dao/AccountDAO.java
@@ -7,29 +7,29 @@ import ru.itis.orisproject.models.Account;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountDAO {
     private final DBConnection dbConnection = DBConnection.getDBConnection();
     private final AccountMapper accountMapper = new AccountMapper();
 
     //language=sql
-    private final String SQL_GET_ACC_BY_USERNAME = "SELECT * FROM accounts WHERE username = ?";
+    private final String SQL_GET_BY_USERNAME = "SELECT * FROM accounts WHERE username = ?";
     //language=sql
-    private final String SQL_SAVE_ACC = """
-INSERT INTO accounts (username, password, email, icon_path, rmmt) VALUES (?, ?, ?, ?, ?)""";
+    private final String SQL_SAVE = """
+INSERT INTO accounts (username, password, email, icon_path) VALUES (?, ?, ?, ?)""";
     //language=sql
     private final String SQL_DELETE_BY_USERNAME = "DELETE FROM accounts WHERE username = ?";
     //language=sql
     private final String SQL_UPDATE_BY_USERNAME = """
-UPDATE accounts SET username = ?, password = ?, email = ?, icon_path = ?, rmmt = ? WHERE username = ?""";
+UPDATE accounts SET username = ?, password = ?, email = ?, icon_path = ? WHERE username = ?""";
     //language=sql
-    private final String SQL_UPDATE_ACCOUNT_SESSION = "UPDATE accounts SET rmmt = ? WHERE username = ?";
-    //language=sql
-    private final String SQL_GET_ACC_BY_SESSION = "SELECT * FROM accounts WHERE rmmt = ?";
+    private final String SQL_GET_BY_USERNAME_ILIKE = "SELECT * FROM accounts WHERE username ILIKE ?";
 
     public Account getByUsername(String username) {
         try {
-            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement(SQL_GET_ACC_BY_USERNAME);
+            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement(SQL_GET_BY_USERNAME);
             preparedStatement.setString(1, username);
             ResultSet resultSet = preparedStatement.executeQuery();
             return resultSet.next() ? accountMapper.mapRow(resultSet) : null;
@@ -40,12 +40,11 @@ UPDATE accounts SET username = ?, password = ?, email = ?, icon_path = ?, rmmt =
 
     public int save(Account acc) {
         try {
-            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement(SQL_SAVE_ACC);
+            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement(SQL_SAVE);
             preparedStatement.setString(1, acc.username());
             preparedStatement.setString(2, acc.password());
             preparedStatement.setString(3, acc.email());
             preparedStatement.setString(4, acc.icon_path());
-            preparedStatement.setString(5, acc.rmmt());
             return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             return -1;
@@ -69,33 +68,25 @@ UPDATE accounts SET username = ?, password = ?, email = ?, icon_path = ?, rmmt =
             preparedStatement.setString(2, acc.password());
             preparedStatement.setString(3, acc.email());
             preparedStatement.setString(4, acc.icon_path());
-            preparedStatement.setString(5, acc.rmmt());
-            preparedStatement.setString(6, username);
+            preparedStatement.setString(5, username);
             return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             return -1;
         }
     }
 
-    public int updateRmmtByUsername(String username, String rmmt) {
+    public List<Account> getByUsernameILike(String username) {
         try {
             PreparedStatement preparedStatement = dbConnection
                     .getConnection()
-                    .prepareStatement(SQL_UPDATE_ACCOUNT_SESSION);
-            preparedStatement.setString(1, rmmt);
-            preparedStatement.setString(2, username);
-            return preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            return -1;
-        }
-    }
-
-    public Account getByRmmt(String rmmt) {
-        try {
-            PreparedStatement preparedStatement = dbConnection.getConnection().prepareStatement(SQL_GET_ACC_BY_SESSION);
-            preparedStatement.setString(1, rmmt);
+                    .prepareStatement(SQL_GET_BY_USERNAME_ILIKE);
+            preparedStatement.setString(1, "\\%%s\\%".formatted(username));
             ResultSet resultSet = preparedStatement.executeQuery();
-            return resultSet.next() ? accountMapper.mapRow(resultSet) : null;
+            List<Account> accs = new ArrayList<>();
+            while (resultSet.next()) {
+                accs.add(accountMapper.mapRow(resultSet));
+            }
+            return accs;
         } catch (SQLException e) {
             return null;
         }
